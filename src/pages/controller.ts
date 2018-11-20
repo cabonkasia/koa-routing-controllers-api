ce2b4115e55189827320ad946271e6554c737697
cabonkasia <cabonkc@gmail.com>
2018-11-20 14:52:55 +0100

    additional get, put and post added

--- src/pages/controller.ts
+++ src/pages/controller.ts
@@ -1,8 +1,15 @@
-import { JsonController, Get, Param } from 'routing-controllers'
+import { JsonController, Get, Put, Param, Body, Post, HttpCode } from 'routing-controllers'
 import pagesById, { Page } from './data'
+
+type PageList = { pages: Page[] }
 
 @JsonController()
 export default class PageController {
+
+    @Get('/pages')
+    allPages(): PageList {
+      return {pages: Object.values(pagesById)}
+    }
 
     @Get('/pages/:id')
     getPage(
@@ -10,4 +17,23 @@ export default class PageController {
     ): Page {
         return pagesById[id]
     }
+
+    @Put('/pages/:id')
+    updatePage(
+    @Param('id') id: number,
+    @Body() body: Partial<Page>
+    ): Page { 
+      console.log(`Incoming PUT body param:`, body)
+    return pagesById[id]
+    }
+
+    @Post('/pages')
+    @HttpCode(201)
+    createPage(
+    @Body() body: Page
+    ): Page {
+    console.log(`Incoming POST body param:`, body)
+    return body
+}
+
 }
